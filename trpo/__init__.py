2e13008ca150f458e4066d3c69af40bcc9d75bef
denklewer <denklewer@gmail.com>
2019-05-17 20:41:41 +0300

    TRPO blank

---
+++ trpo/__init__.py
@@ -0,0 +1,7 @@
+from trpo import TRPOTrainer, DEFAULT_CONFIG
+from ray.rllib.agents.ppo.appo import APPOTrainer
+from ray.rllib.utils import renamed_class
+
+TRPOAgent = renamed_class(TRPOTrainer)
+
+__all__ = ["TRPOAgent", "APPOTrainer", "TRPOTrainer", "DEFAULT_CONFIG"]
