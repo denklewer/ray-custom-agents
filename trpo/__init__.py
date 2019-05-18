4a752914461179c395ac847518be1855142fe21b
denklewer <denklewer@gmail.com>
2019-05-18 22:03:49 +0300

    import correction

---
+++ trpo/__init__.py
@@ -0,0 +1,8 @@
+from trpo.trainer import TRPOTrainer, DEFAULT_CONFIG
+from trpo_policy_graph import TRPOPolicyGraph
+from ray.rllib.agents.ppo.appo import APPOTrainer
+from ray.rllib.utils import renamed_class
+
+TRPOAgent = renamed_class(TRPOTrainer)
+
+__all__ = ["TRPOPolicyGraph","TRPOAgent", "APPOTrainer", "TRPOTrainer", "DEFAULT_CONFIG"]
